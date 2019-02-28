5d25e592d034a100c872e5875a671b7946534bae
DackLi <244998907@qq.com>
2019-02-28 15:22:20 +0800

    添加webpack配置文件

---
+++ ym-admin/vue.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  // 文件输入位置
+  publicPath: '/',
+  // 输入目录文件夹
+  outputDir: 'dist',
+  // 设置输出eslint语法错误
+  lintOnSave: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://www.easy-mock.com/mock/5c6525a7d758c80490517aba/vue3', // 对应自己的接口
+        changeOrigin: true,
+        ws: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
+}
